309fea90a3c9c33d32c69b389f2eafc426f6d6eb
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2022-08-06 08:58:00 +0900

    [Silver III] Title: N과 M (6), Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,28 @@
+# 백트래킹으로 풀기
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+Ns = list(map(int, sys.stdin.readline().split()))
+Ns.sort()
+
+s = []  # stack
+
+def dfs():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+
+    if len(s) == 0:
+        z = 0
+    else:
+        z = max(s)
+
+    for i in Ns: # 수열 Ns안에 있는 수들
+        if (i not in s) and i > z:
+            s.append(i)
+            dfs()
+            s.pop()
+
+
+dfs()
